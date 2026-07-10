150271d2776135630c3759408eb476eb3ef19bbb
KishoreXtobi <kishorekr3450@gmail.com>
2026-07-10 11:29:34 +0530

    feat: implement application routing architecture and base layouts

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,67 @@
 import React from 'react'
-
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import {
+  About,
+  Cart,
+  Checkout,
+  Error,
+  Homelayout,
+  Landing,
+  Login,
+  Orders,
+  Products,
+  Register,
+  Singleproduct,
+} from './pages'
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Homelayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+      },
+      {
+        path: 'products',
+        element: <Products />,
+      },
+      {
+        path: 'products/:id',
+        element: <Singleproduct />,
+      },
+      {
+        path: 'cart',
+        element: <Cart />,
+      },
+      {
+        path: 'about',
+        element: <About />,
+      },
+      {
+        path: 'checkout',
+        element: <Checkout />,
+      },
+      {
+        path: 'orders',
+        element: <Orders />,
+      },
+    ],
+  },
+  {
+    path: 'login',
+    element: <Login />,
+    errorElement: <Error />,
+  },
+  {
+    path: 'register',
+    element: <Register />,
+    errorElement: <Error />,
+  },
+])
 const App = () => {
-  return <div>App</div>
+  return <RouterProvider router={router} />
 }
 
 export default App
